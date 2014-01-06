2d6eaf0265c012e83b9068d56f96d07985157c03
Bernard Notarianni <bernard.notarianni@gmail.com>
2014-01-06 08:45:51 +0100

    Add a repl

---
+++ LissenRepl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lissen;
+
+namespace LissenRepl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lissen Lisp");
+            Console.WriteLine("");
+            Console.WriteLine("<ctrl>-C to exit.");
+
+            Evaluator evaluator = new Evaluator();
+            Parser parser = new Parser();
+
+            while(true)
+            {
+                Console.Write(">");
+                string userInput = Console.ReadLine();
+
+                string result = evaluator.Eval(parser.Parse(userInput)).ToString();
+
+                Console.WriteLine(result);
+            }            
+        }
+    }
+}
